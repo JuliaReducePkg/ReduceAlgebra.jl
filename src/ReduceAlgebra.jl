e7ad4d33c9a44245a8cd73c9747524185081f5f2
Michael Reed <michaelreed@crucialflow.com>
2018-04-23 21:04:40 -0400

    added Reduce and ReduceLinAlg

--- src/ReduceAlgebra.jl
+++ src/ReduceAlgebra.jl
@@ -1,5 +1,12 @@
+__precompile__()
 module ReduceAlgebra
 
-# package code goes here
+#   This file is part of ReduceAlgebra.jl. It is licensed under the MIT license
+#   Copyright (C) 2018 Michael Reed
+
+using Reduce
+using ReduceLinAlg
+
+__init__() = nothing
 
 end # module
